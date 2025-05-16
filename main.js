b7e26750de54c90067a0f70dc382fded9f443450
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-16 10:25:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const { app, BrowserWindow } = require('electron');
+const bcrypt = require('bcrypt')
 const path = require('path')
-const fs = require('fs')
+const fs = require('fs/promises')
 
 require('electron-reload')(__dirname, {
   electron: require(`${__dirname}/node_modules/electron`)
 });
 
-function createMainWindow(){
+function createMainWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 800,
@@ -18,6 +19,26 @@ function createMainWindow(){
 
   win.loadFile('register.html')
 }
+
+const register = () => {
+
+}
+const login = async (credentials) => {
+  try {
+    const response = await fs.readFile('./credentials/users.json', 'utf-8')
+    const users = JSON.parse(response)
+    const isValidUser = users.find((user) => user.email === 'ee@example.com' && user.password === 'teste123') ?? false
+    console.log(isValidUser)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+login()
+
+const logout = () => {
+
+}
+
 
 app.whenReady().then(createMainWindow)
 
