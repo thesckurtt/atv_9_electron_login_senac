422187bc3d4d51e5ae57ce340195186e9749d498
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-16 11:05:21 -0300

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,18 +1,34 @@
 
 async function login() {
-  const inptEmailLogin = document.getElementById('email').value
-  const inptPasswordLogin = document.getElementById('password').value
+  const inptEmail = document.getElementById('email').value
+  const inptPassword = document.getElementById('password').value
 
   const credentials = {
-    "email": inptEmailLogin,
-    "password": inptPasswordLogin,
+    "email": inptEmail,
+    "password": inptPassword,
   }
-  if (inptEmailLogin && inptPasswordLogin) {
+  if (inptEmail && inptPassword) {
     const res = await window.authAPI.login(credentials)
     if (res.error) alert('credênciais inválidas')
 
   }
 }
 
-async function register(){
+async function register() {
+  const inptName = document.getElementById('name').value
+  const inptEmail = document.getElementById('email').value
+  const inptPassword = document.getElementById('password').value
+
+  const user = {
+    "name": inptName,
+    "email": inptEmail,
+    "password": inptPassword
+  }
+
+  if(inptName && inptEmail && inptPassword){
+    const response = window.authAPI.register(user)
+    if(response.error){
+      alert("Erro ao cadastrar")
+    }
+  }
 }
