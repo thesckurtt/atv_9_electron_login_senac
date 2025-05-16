90a9685c2f00353d1c2611b432e60efac5c9ad86
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-16 10:59:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const bcrypt = require('bcrypt')
 const path = require('path')
 const fs = require('fs/promises')
 
+let mainWindow = undefined;
 require('electron-reload')(__dirname, {
   electron: require(`${__dirname}/node_modules/electron`)
 });
 
 function createMainWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 800,
     webPreferences: {
@@ -17,28 +18,37 @@ function createMainWindow() {
     }
   })
 
-  win.loadFile('register.html')
+  mainWindow.loadFile('login.html')
 }
 
 const register = () => {
 
 }
+
 const login = async (credentials) => {
   try {
     const response = await fs.readFile('./credentials/users.json', 'utf-8')
     const users = JSON.parse(response)
-    const isValidUser = users.find((user) => user.email === 'ee@example.com' && user.password === 'teste123') ?? false
+    const isValidUser = users.find((user) => user.email === credentials.email && user.password === credentials.password) ?? false
     console.log(isValidUser)
+    if (isValidUser) {
+      mainWindow.loadFile('index.html')
+    } else {
+      return { error: true }
+    }
   } catch (error) {
     throw new Error(error)
   }
 }
-login()
+
 
 const logout = () => {
 
 }
 
+ipcMain.handle('login', async (_, credentials) => {
+  return login(credentials)
+})
 
 app.whenReady().then(createMainWindow)
 
